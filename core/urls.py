2ed3debaec5d30ee8270c73cacf78ff0b689fa3a
ssorin <ssorin@dwdlc.com>
2022-02-28 15:12:01 +0100

    Mediatheque ===========
    Etape 2 :
    ---------
    
    - Création de l'application biblio
    - Création du models avec les différentes tables et quelques methodes/propriété utiles (get_absolute_url, image)
    - creation des vues liste et detail pour l'ensemble des tables
    - creation des templates liste et detail pour l'ensemble des tables
    - administration de l'ensemble des tables

--- core/urls.py
+++ core/urls.py
@@ -23,6 +23,6 @@ from .views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('biblio/', include('biblio.urls')),
+    path('collection/', include('biblio.urls')),
     path('', home, name='home'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

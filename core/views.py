a0ce45fade2c8999a7ce58e0b338e07439ac54b4
ssorin <ssorin@dwdlc.com>
2022-03-14 16:20:39 +0100

    Mediatheque ===========
    - login / logout

--- core/views.py
+++ core/views.py
@@ -13,4 +13,4 @@ def home(request):
     books = Book.objects.all()
     last_books_add = books.order_by('-create_date')[:5]
 
-    return render(request, "home.html", {'last_books':last_books_add, 'books': books})
+    return render(request, "home.html", {'last_books': last_books_add, 'books': books})

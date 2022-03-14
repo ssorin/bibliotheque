3b23da2fa735a72089017457717f48fabecfb402
ssorin <ssorin@dwdlc.com>
2022-03-14 16:20:20 +0100

    Mediatheque ===========
    - login / logout

--- core/views.py
+++ core/views.py
@@ -1,9 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import authenticate, login
-from django.contrib.auth import logout
-from django.conf import settings
 
 from biblio.models import Book
 
